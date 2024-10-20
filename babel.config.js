5f854a123e27753419396821ab8fac782beacf2e
Pavaman Dabeer <dpavaman@Pavamans-MacBook-Air.local>
2024-10-20 12:59:32 +0530

    fix: added presets and plugins to babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,18 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    ['module:react-native-dotenv', {
-      moduleName: '@env',
-      path: '.env',  // default path for fallback
-      blocklist: null,
-      allowlist: null,
-      safe: false,
-      allowUndefined: true,
-    }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blocklist: null,
+        allowlist: null,
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
   ],
 };
