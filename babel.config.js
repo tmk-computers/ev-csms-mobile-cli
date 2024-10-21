2e77324c2400dad20275d8de004c9ef2c1d9b2ab
Avadhut Kore <amkore01@gmail.com>
2024-10-21 11:34:35 +0530

    fixed map marker breaking issue

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,3 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    ['@babel/plugin-transform-private-methods', { loose: true }],
-    ['@babel/plugin-transform-class-properties', { loose: true }],
-    [
-      'module:react-native-dotenv',
-      {
-        moduleName: '@env',
-        path: '.env',
-        blocklist: null,
-        allowlist: null,
-        safe: false,
-        allowUndefined: true,
-      },
-    ],
-  ],
 };
