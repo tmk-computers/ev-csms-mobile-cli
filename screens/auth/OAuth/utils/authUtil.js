edf8a558ee29978f393c0321f6a2248319831d77
Dhananjaya Dabeer <dabeerdhananja123@gmail.com>
2024-10-23 12:44:04 +0530

    Fixes in google signin

--- screens/auth/OAuth/utils/authUtil.js
+++ screens/auth/OAuth/utils/authUtil.js
@@ -1,23 +1,32 @@
 import { GoogleSignin } from "@react-native-google-signin/google-signin"
 import { mapSocialUserToProfile } from "../../../../utils/utility"
+import { Alert } from "react-native";
+
 
 const authUtil = {
 
     configGoogleAuth: ({ webClientId }) => {
         GoogleSignin.configure({
+            offlineAccess: true,
             webClientId
         })
     },
 
     signinUsingGoogle: async () => {
         try {
-            console.log('✅ Signing in to Google')
+            console.log('✅ Signing in to Google');
             await GoogleSignin.hasPlayServices();
-            const response = await GoogleSignin.signIn();
+            const response = await GoogleSignin.signIn();    
+            
+            if (!response.data.user || !response.data.user.id || !response.data.user.email) {
+                throw new Error("User object is invalid or missing necessary fields.");
+            }
+    
             const userInfo = mapSocialUserToProfile(response.data.user);
-            return { ...userInfo, type: 'external', service: 'Google' }
+            return { ...userInfo, type: 'external', service: 'Google' };
         } catch (error) {
             console.error('Google Sign-In Error:', error);
+            Alert.alert("Sign-In Error", error.message || "An unknown error occurred.");
             throw error;
         }
     },
